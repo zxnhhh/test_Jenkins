32d0df411a421da19341235770e6ef5b5331479b
zxnhhh <zhangxiangnan_love@163.com>
2021-07-13 16:06:52 +0800

    test 'error' and 'warnig' in log parser

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ echo "second test for push trigger Jenkins build..."
 echo "third test for push trigger Jenkins build..."
 echo "fourth test for push trigger Jenkins build..."
 echo "fifth test for push trigger Jenkins build..."
+echo "error"
+echo "warning"
+echo "Warning"
+echo "WARNING"
 error
